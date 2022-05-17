ec9973a4755cb9f845f2df7b1d3048cc5bf416ad
Aravindan <uma.aravindan@nokia.com>
2022-05-17 16:19:30 +0530

    first commit

---
+++ arrays.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class arrays{
+
+   public static void main(String[] args)
+   {
+	   String students[];
+	   students = new String[4];
+	    
+       String students1[]={"Henry","Shane","Sharma","Watson"};
+       System.out.println(students1[0]+" "+ students1[1]);
+	   System.out.println(students1[2]+" "+students1[1]);
+   }
+   
+   
+   
+   }
+   
